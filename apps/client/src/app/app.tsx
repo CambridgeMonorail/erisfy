4b0853546ca4928d2d366dfd4d6cb55d116537c3
Tim Morris <TimDMorris@users.noreply.github.com>
2025-02-04 08:26:28 +0000

    refactor(app): update ErrorBoundary import path

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -2,7 +2,7 @@ import { FC, useEffect } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { Toaster } from '@erisfy/shadcnui';
 import { navigationConfig } from './constants/navigationConfig';
-import { ErrorBoundary } from '../../../../libs/shadcnui-blocks/src/lib/components/error-boundary';
+import { ErrorBoundary } from '@erisfy/shadcnui-blocks';
 import { OnboardingFlow } from './pages/onboarding/OnboardingFlow';
 
 const App: FC = () => {
